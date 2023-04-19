8bb4094a7d3e7dde4938413b42abae9f06a42d70
RAJASRI-G <125953572+RAJASRI-G@users.noreply.github.com>
2023-04-19 12:50:26 +0530

    Add files via upload

--- CC2/src/test/java/cc2/PART_B.java
+++ CC2/src/test/java/cc2/PART_B.java
@@ -2,30 +2,49 @@ package cc2;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class PART_B {
-	public static void main(String args[])
-	{
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver;
-		ChromeOptions co = new ChromeOptions();
-		co.addArguments("--remote-allow-origins=*");
-		driver = new ChromeDriver(co);
-		driver.get("https://www.yatra.com/hotels");
-		driver.manage().window().maximize();
-		WebElement flight=driver.findElement(By.xpath("//*[@id=\"booking_engine_flights\"]"));
-    	flight.click();
-    	WebElement round=driver.findElement(By.xpath("//*[@id=\"BE_flight_form_wrapper\"]/div[1]/div[1]/ul[1]/li[2]/a"));
-    	round.click();
-    	WebElement date=driver.findElement(By.xpath("//*[@id=\"BE_flight_arrival_date\"]"));
-    	date.click();
-    	WebElement box=driver.findElement(By.xpath("//*[@id=\"BE_flight_form_wrapper\"]/div[3]/div[1]/div[1]/a/i"));
-    	box.click();
-    	driver.get("https://flight.yatra.com/air-search-ui/dom2/trigger?type=R&viewName=normal&flexi=0&noOfSegments=2&origin=DEL&originCountry=IN&destination=BOM&destinationCountry=IN&flight_depart_date=20%2F04%2F2023&arrivalDate=28%2F04%2F2023&ADT=1&CHD=0&INF=0&non_stop=1&class=Economy&source=fresco-home&unqvaldesktop=688441000721");
+		  WebDriver d;
+		  @Test
+		  public void f() throws InterruptedException {
+			  ChromeOptions co=new ChromeOptions();
+			  co.addArguments("--remote-allow-origins=*");
+			  WebDriverManager.chromedriver().setup();
+			  d=new ChromeDriver(co);
+			  d.get("https://www.yatra.com/flights");
+			  d.manage().window().maximize();
+			  Thread.sleep(4000);
+			  d.findElement(By.xpath("//*[@id=\"booking_engine_flights\"]")).click();
+			  Thread.sleep(2000);
+			  d.findElement(By.xpath("//*[@id=\"BE_flight_form_wrapper\"]/div[1]/div[1]/ul[1]/li[2]/a")).click();
+			  Thread.sleep(2000);
+			  d.findElement(By.xpath("//*[@id=\"BE_flight_origin_city\"]")).click();
+			  Thread.sleep(2000);
+			  d.findElement(By.xpath("//*[@id=\"BE_flight_form_wrapper\"]/div[1]/div[2]/ul/li[1]/ul/li[1]/div/div/ul/div/div/div/li[3]")).click();
+			  Thread.sleep(2000);
+			  d.findElement(By.xpath("//*[@id=\"BE_flight_form_wrapper\"]/div[1]/div[2]/ul/li[1]/ul/li[3]/div/div/ul/div/div/div/li[4]")).click();
+			  Thread.sleep(2000);
+			  d.findElement(By.xpath("//*[@id=\"BE_flight_origin_date\"]")).click();
+			  Thread.sleep(2000);
+			  d.findElement(By.xpath("//*[@id=\"20/04/2023\"]")).click();
+			  Thread.sleep(2000);
+			  d.findElement(By.xpath("//*[@id=\"BE_flight_arrival_date\"]")).click();
+			  Thread.sleep(2000);
+			  d.findElement(By.xpath("//*[@id=\"24/04/2023\"]")).click();
+			  Thread.sleep(2000);
+			  d.findElement(By.xpath("//*[@id=\"BE_flight_paxInfoBox\"]/i")).click();
+			  Thread.sleep(2000);
+			  d.findElement(By.xpath("//*[@id=\"BE_flight_passengerBox\"]/div[1]/div[1]/div/div/span[2]")).click();
+			  Thread.sleep(2000);
+			  d.findElement(By.xpath("//*[@id=\"flight_class_select_child\"]/ul/li[3]")).click();
+			  Thread.sleep(2000);
+			  d.findElement(By.xpath("//*[@id=\"BE_flight_form_wrapper\"]/div[3]/div[1]/div[1]/a/i")).click();
+			  Thread.sleep(2000);
+			  d.findElement(By.xpath("//*[@id=\"BE_flight_flsearch_btn\"]")).click();
 	}
-}
+	}
